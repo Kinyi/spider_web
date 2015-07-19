743b60ed52b0de83ef2ba4827979cdeb555e6a19
Kinyi <qq554071942@sina.com>
2015-07-20 07:55:07 +0800

    2015/07/20

---
+++ src/main/java/cn/crxy/spider/web/index/SolrIndex.java
@@ -0,0 +1,62 @@
+package cn.crxy.spider.web.index;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import cn.crxy.spider.web.domain.Goods;
+import cn.crxy.spider.web.utils.HbaseUtils;
+import cn.crxy.spider.web.utils.RedisUtils;
+import cn.crxy.spider.web.utils.SleepUtils;
+import cn.crxy.spider.web.utils.SolrUtil;
+
+/**
+ * 建立商品索引，
+ * 从redis中查询需要建索引的商品ID
+ * 然后查询hbase，建立索引
+ * 如果建立索引的时候出现异常，则把当前商品id重新放入索引表中
+ *
+ */
+public class SolrIndex{
+	static final Logger logger = LoggerFactory.getLogger(SolrIndex.class);
+	private static final String SOLR_INDEX = "solr_index";
+	static RedisUtils redis = new RedisUtils();
+	/**
+	 * 建立索引
+	 */
+	public static void doIndex(){
+		String goodsId = "";
+		try {
+			while (!Thread.currentThread().isInterrupted()) {
+				goodsId = redis.poll(SOLR_INDEX);
+				if(StringUtils.isNotBlank(goodsId)){
+					Goods goods = HbaseUtils.get(HbaseUtils.TABLE_NAME, goodsId);
+					if(goods==null){
+						logger.error("id为{}的商品索引建立失败!原因：没有在hbase数据库中查询到数据", goodsId);
+					}else{
+						if(goodsId.startsWith("jd_")){
+							goods.setFrom("京东");
+						}else if(goodsId.startsWith("yx_")){
+							goods.setFrom("易迅");
+						}else{
+							goods.setFrom("未知");
+						}
+						System.out.println(goods.toString());
+						SolrUtil.addIndex(goods);
+					}
+				}else{
+					System.out.println("暂时没有需要索引的数据,休息一会");
+					SleepUtils.sleep(5000);
+				}
+			}
+		} catch (Exception e) {
+			logger.error("id为{}的商品索引建立失败!{}", goodsId, e);
+			redis.add(SOLR_INDEX, goodsId);
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		doIndex();
+	}
+}
